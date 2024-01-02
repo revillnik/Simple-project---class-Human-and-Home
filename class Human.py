4fdd36269754cf4b2a091e50d6a0696b1f73c6eb
revillnik <revillnik@gmail.com>
2024-01-02 13:40:09 +0300

    last

--- class Human.py
+++ class Human.py
@@ -2,9 +2,25 @@ class Human:
     default_name = "Иван"
     default_age = 30
 
-    def __init__(self, name=default_name, age=default_age):
+    def __init__(self, money, house, name=default_name, age=default_age):
         self.name = name
         self.age = age
+        self.__money = money
+        self.__house = house
+        self.get_numberhouses()
+
+    def get_numberhouses(self):
+        if self.__house in "yesYESYes":
+            self.__numberhouses = 1
+            return self.__numberhouses
+        else:
+            self.__numberhouses = 0
+            return self.__numberhouses
+
+    def private_info(self):
+        print(
+            f"Покупатель: {self.name}, {self.age} лет, общее количество денег = {self.__money}, наличие дома: {self.__house}, количество домов: {self.__numberhouses}"
+        )
 
     def info(self):
         print(f"Покупатель: {self.name}, {self.age} лет")
@@ -16,14 +32,24 @@ class Human:
     @staticmethod
     def pay_house(money, price):
         if money >= price:
-            print(f"Вы можете купить дом, и у вас останется {money - price} рублей")
+            print(f"Вы купили дом, и у вас осталось {money - price} рублей")
         else:
             print("Вы не можете его купить, необходимо больше золота")
 
-    def buy_house(self, money=0, price=1000000):
+    def buy_house(self, price=1000000):
         print("Дом стоит", price, "рублей")
-        return self.pay_house(money, price)
+        return self.pay_house(self.__money, price)
 
-Nikita = Human('Никита', 25)
+    def private_house(self):
+        self.__numberhouses += 1
+
+
+print(
+    "Введите необходимую информацию: количество денег, наличие дома, имя, возраст, приватные данные будут засекречены"
+)
+Nikita = Human(10000000, "yes", "Никита", 25)
 Nikita.info()
-Nikita.buy_house(1500000)
+Nikita.private_info()
+Nikita.buy_house()
+Nikita.private_house()
+Nikita.private_info()
